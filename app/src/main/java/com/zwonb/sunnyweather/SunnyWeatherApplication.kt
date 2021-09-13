ZhouZhanTong/SunnyWeather-1

实现搜索全球城市数据功能

app/src/main/java/com/zwonb/sunnyweather/SunnyWeatherApplication.kt
@@ -1,0 +1,25 @@
+package com.zwonb.sunnyweather
+
+import android.app.Application
+import android.content.Context
+
+/**
+ * application
+ *
+ * @author zwonb
+ * @date 2020/4/11
+ */
+
+class SunnyWeatherApplication : Application() {
+
+    companion object {
+        lateinit var context: Context
+        const val TOKEN = "EGVFUV25lRu5Y0tt"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        context = applicationContext
+    }
+}

zwonb/SunnyWeather

app/src/main/java/com/zwonb/sunnyweather/SunnyWeatherApplication.kt
@@ -1,0 +1,25 @@
+package com.zwonb.sunnyweather
+
+import android.app.Application
+import android.content.Context
+
+/**
+ * application
+ *
+ * @author zwonb
+ * @date 2020/4/11
+ */
+
+class SunnyWeatherApplication : Application() {
+
+    companion object {
+        lateinit var context: Context
+        const val TOKEN = "TODO"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        context = applicationContext
+    }
+}